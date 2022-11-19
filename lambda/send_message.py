d4d7f81d38cd1b8ab2d4864c580a0f58e11190e6
Brennan McMicking <brennanmcmicking@gmail.com>
2022-11-19 14:05:57 -0800

    feat(code): add initial lambda function code

--- lambda/send_message.py
+++ lambda/send_message.py
@@ -1,7 +1,39 @@
 import json
+from common import *
 
 def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps('Hello World')
-    }
+    try:
+        body = json.loads(event.get('body'))
+    except:
+        return bad_request("failed to decode request body")
+
+    username = body.get('username')
+    token = body.get('cookie')
+    recipient = body.get('to')
+    message = body.get('message')
+    
+    if verify_token(username, token):
+        item = dynamodb_table.get_item(Key={ "userId": recipient }).get('Item')
+        msg_queue = json.loads(item.get("messageQueue"))
+        msg_queue[str(datetime.utcnow())] = json.dumps({
+            'message': message,
+            'sender': username,
+        })
+        res = dynamodb_resource.update_item(
+            Key={
+                'userId': item.get('userId'),
+            },
+            UpdateExpression='set messageQueue=:d',
+            ExpressionAttributeValues={
+                ':d': json.dumps(msg_queue)
+            },
+        )
+        if not res:
+            return server_error("database update failed")
+        
+        return {
+            "statusCode": 200,
+            "body": 'updated message queue'
+        }
+
+    return bad_request("failed to verify cookie")
