0b23933a4388ec92ce32b01cdcefc97ad691bf2e
Brennan Wilkes <brennan@codexwilkes.com>
2022-11-20 11:28:58 -0800

    chore: Update URL

--- lambda/create_account.py
+++ lambda/create_account.py
@@ -58,7 +58,7 @@ def verify_token(username, token):
 
 def lambda_handler(event, context):
     """
-    Handler for calls to https://c11ipox830.execute-api.us-west-2.amazonaws.com/seng360/create_account
+    Handler for calls to https://ads5u2p9gb.execute-api.us-west-2.amazonaws.com/seng360/create_account
     """
     body = event.get("body")
     if type(body) is str:
